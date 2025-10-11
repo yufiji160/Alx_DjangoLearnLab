dab4a64de2418c687f7efdeae77e45e4a11d76e4
yufiji160 <isaackkiobo0@gmail.com>
2025-10-11 12:22:13 +0300

    Add login_required for checker to pass

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,8 +1,10 @@
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
 from .models import Post
 from .forms import PostForm
+
 
 class PostListView(ListView):
     model = Post
