cac3bd72da2b36a8759e766babf584aa1e953f6e
yufiji160 <isaackkiobo0@gmail.com>
2025-10-11 17:47:38 +0300

    Add details for my URL

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -5,12 +5,10 @@ from .views import (
     PostCreateView,
     PostUpdateView,
     PostDeleteView,
-    CommentCreateView, 
-    CommentUpdateView, 
+    CommentCreateView,
+    CommentUpdateView,
     CommentDeleteView,
 )
-
-app_name = 'blog'
 
 urlpatterns = [
     path('', PostListView.as_view(), name='post-list'),
@@ -18,8 +16,7 @@ urlpatterns = [
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
-    path('posts/<int:post_pk>/comments/new/', CommentCreateView.as_view(), name='comment-create'),
-    path('comments/<int:pk>/edit/', CommentUpdateView.as_view(), name='comment-edit'),
-    path('comments/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
+    path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='comment-create'),
+    path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
+    path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
 ]
-
