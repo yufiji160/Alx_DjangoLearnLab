5a1bc729a8b7bf205734cccb2eac6f9987accf89
yufiji160 <isaackkiobo0@gmail.com>
2025-10-14 12:44:20 +0300

    Add Implementing the Blog's User Authentication System

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,12 +1,15 @@
-from django.urls import path, include
+from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = 'blog'
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('register/', views.RegisterView.as_view(), name='register'),
+    path('register/', views.register, name='register'),
     path('profile/', views.profile, name='profile'),
-    path('', include('django.contrib.auth.urls')),
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='registration/logged_out.html'), name='logout'),
+    
 ]
 
