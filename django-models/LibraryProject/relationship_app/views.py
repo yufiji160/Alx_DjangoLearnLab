8f075ef6d1aa4c746c8bd061dd25f4b7b65709a1
yufiji160 <isaackkiobo0@gmail.com>
2025-10-15 14:41:33 +0300

    Add Django Views and URL Configuration

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
-from .models import Book, Library
+from .models import Book, Library 
 
 def list_books(request):
     books = Book.objects.all()
