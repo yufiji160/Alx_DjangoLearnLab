e345e4ecff7e130f81f8bbdccb6f7cb0f741f6d1
yufiji160 <isaackkiobo0@gmail.com>
2025-10-16 13:07:22 +0300

    Add Implementing a Custom User Model in Django

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,10 +1,9 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
-class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
-    publication_year = models.IntegerField()
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
 
     def __str__(self):
-        return f"{self.title} by {self.author} ({self.publication_year})"
+        return self.username
