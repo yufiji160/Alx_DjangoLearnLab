d245f0279e656925eefed75e64f7f3ab3f362480
yufiji160 <isaackkiobo0@gmail.com>
2025-10-16 12:33:49 +0300

    Add add_book and edit_book URL patterns

--- django-models/LibraryProject/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/LibraryProject/relationship_app/urls.py
@@ -1,10 +1,7 @@
 from django.urls import path
 from . import views
-from django.urls import path, include
 
 urlpatterns = [
-    path('admin-view/', views.admin_view, name='admin_view'),
-    path('librarian-view/', views.librarian_view, name='librarian_view'),
-    path('member-view/', views.member_view, name='member_view'),
-    path('', include('relationship_app.urls')),
+    path('add_book/', views.add_book, name='add_book'),
+    path('edit_book/<int:book_id>/', views.edit_book, name='edit_book'),
 ]
