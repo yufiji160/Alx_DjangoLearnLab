327d9acb83d158a63effc98f01f42e41cb3b5df6
yufiji160 <isaackkiobo0@gmail.com>
2025-10-17 16:53:35 +0300

    Add Implementing Posts and Comments Functionality

--- social_media_api/accounts/admin.py
+++ social_media_api/accounts/admin.py
@@ -1,10 +1,5 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
+from .models import User
 
-
-@admin.register(CustomUser)
-class CustomUserAdmin(UserAdmin):
-    fieldsets = UserAdmin.fieldsets + (
-      ('Additional', {'fields': ('bio', 'profile_picture', 'followers')}),
-)
+admin.site.register(User, UserAdmin)
