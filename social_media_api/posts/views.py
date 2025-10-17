1dc27b59b5c89932673abc75df711fba9282397c
yufiji160 <isaackkiobo0@gmail.com>
2025-10-17 17:08:33 +0300

    Add Implementing Posts and Comments Functionality

---
+++ social_media_api/posts/views.py
@@ -0,0 +1,39 @@
+from rest_framework import viewsets, permissions, filters
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
+
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to only allow owners of an object to edit or delete it.
+    """
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.author == request.user
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    """
+    Provides CRUD operations for Post model.
+    """
+    queryset = Post.objects.all().order_by('-created_at')
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title', 'content']
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    """
+    Provides CRUD operations for Comment model.
+    """
+    queryset = Comment.objects.all().order_by('-created_at')
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
